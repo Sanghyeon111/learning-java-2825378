c7e3cb9eeb153d810a4a5e22d0590c0a04bc8267
Sang Lee <tkdgus2233440@gmail.com>
2023-12-17 02:43:00 -0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,11 @@ public class Main {
         String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
-
         // Write a print statement giving the answer choices
-
         // Have the user input an answer
         // Retrieve the user's input
-
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
-
         // If the user's input does not match the correctAnswer...
         // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
 
